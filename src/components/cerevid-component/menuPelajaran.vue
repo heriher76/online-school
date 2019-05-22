d3279c8fb03b4a72a9a5bf62d1605402d77c72ae
Chakra <cakrabernard@gmail.com>
2019-05-23 01:43:59 +0700

    update cerevid

---
+++ src/components/cerevid-component/menuPelajaran.vue
@@ -0,0 +1,12 @@
+<template>
+	<v-toolbar
+		height="40"
+	>
+		<v-toolbar-items class="hidden-sm-and-down">
+		    <v-btn to="/cerevid/detail-pelajaran/ikhtisar" flat color="#2c3e50"  style="text-decoration: none">Ikhtisar</v-btn>
+		    <v-btn to="/cerevid/detail-pelajaran/kurikulum" flat color="#2c3e50"  style="text-decoration: none">Kurikulum</v-btn>
+		    <v-btn to="/cerevid/detail-pelajaran/ulasan" flat color="#2c3e50"  style="text-decoration: none">Ulasan</v-btn>
+		    <v-btn to="/cerevid/detail-pelajaran/profil-pengajar" flat color="#2c3e50"  style="text-decoration: none">Profil Pengajar</v-btn>
+		</v-toolbar-items>
+    </v-toolbar>
+</template>
--- src/views/cerevid/tambahPelajaran.vue
+++ src/views/cerevid/tambahPelajaran.vue
@@ -7,47 +7,13 @@
 		<v-container fluid>
 				<v-layout row wrap>
 					<v-flex xs12 sm12 md3>
-						<v-card-text style="padding-top:0">
-						<v-toolbar flat dark>
-							<v-list  style="background-color:#c62828">
-								<v-list-tile>
-									<v-list-tile-title class="title">
-										Kelola Pelajaran
-									</v-list-tile-title>
-								</v-list-tile>
-							</v-list>
-						</v-toolbar>
-						<v-list dense class="pt-0">
-							<v-list-tile
-								@click="daftarPelajaran"
-							>
-								<v-list-tile-action>
-									<v-icon>dashboard</v-icon>
-								</v-list-tile-action>
-
-								<v-list-tile-content>
-									<v-list-tile-title>Daftar Pelajaran</v-list-tile-title>
-								</v-list-tile-content>
-							</v-list-tile>
-							<v-list-tile
-								@click="tambahPelajaran"
-							>
-								<v-list-tile-action>
-									<v-icon>dashboard</v-icon>
-								</v-list-tile-action>
-
-								<v-list-tile-content>
-									<v-list-tile-title>Tambah Pelajaran</v-list-tile-title>
-								</v-list-tile-content>
-							</v-list-tile>
-						</v-list>
-					</v-card-text>
+    					<sidebarGuru />
 					</v-flex>
 					<v-flex xs12 sm12 md9>
               <v-card-text style="padding-top:0">
               <v-toolbar
                 card
-                color="red darken-4"
+                color="#353b48"
                 dark
               >
                 <v-toolbar-title>Submit a post</v-toolbar-title>
@@ -82,10 +48,12 @@
 </template>
 <script>
 	import subNavbarGuru from '../../components/cerevid-component/subNavbarGuru'
+	import sidebarGuru from '../../components/cerevid-component/sidebarGuru'
 	export default {
 		name:"tambah-pelajaran",
 		components:{
 			subNavbarGuru,
+			sidebarGuru
 		},
     data () {
       return {
