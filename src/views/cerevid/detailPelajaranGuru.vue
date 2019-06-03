25266a38c36685006142f260d3576a788103dbc7
Heri Hermawan <herhermawan007@gmail.com>
2019-06-04 05:12:32 +0700

    add email notif

--- src/views/cerevid/detailPelajaranGuru.vue
+++ src/views/cerevid/detailPelajaranGuru.vue
@@ -49,17 +49,12 @@
                             <v-layout row wrap>
                               <div>
                                 <div class="headline">
-                                  <div>Bahasa PHP dilengkapi tutorial 2019</div>
+                                  <div>{{ dataDetailPelajaran.data.title }}</div>
                                 </div>
                                 <v-container fluid>
                                   <v-layout>
                                     <v-flex>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        {{ dataDetailPelajaran.data.description }}
                                     </v-flex>
                                   </v-layout>
                                 </v-container>
